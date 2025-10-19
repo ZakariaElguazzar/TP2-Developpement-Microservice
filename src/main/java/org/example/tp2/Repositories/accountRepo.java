f7e9d61b999f484725220127d96d997d757e323f
zakaria-el-guazzar <elguazzarzakaria10@gmail.com>
2025-10-19 01:52:15 +0100

    Renaming CompteRepo into accountRepo

--- src/main/java/org/example/tp2/Repositories/accountRepo.java
+++ src/main/java/org/example/tp2/Repositories/accountRepo.java
@@ -1,10 +1,10 @@
 package org.example.tp2.Repositories;
 
-import org.example.tp2.Entities.Compte;
+import org.example.tp2.Entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompteRepo extends JpaRepository<Compte,String> {
+public interface accountRepo extends JpaRepository<Account,String> {
 
 }
