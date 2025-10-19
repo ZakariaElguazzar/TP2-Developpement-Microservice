b0353ffcbf2e0080a1c50cff351bdd9a2d7fe032
zakaria-el-guazzar <elguazzarzakaria10@gmail.com>
2025-10-19 03:30:48 +0100

    Adding the JsonProperty for the enums values

--- src/main/java/org/example/tp2/Enums/accountType.java
+++ src/main/java/org/example/tp2/Enums/accountType.java
@@ -1,6 +1,10 @@
 package org.example.tp2.Enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum accountType {
+    @JsonProperty("SAVING")
     SAVING,
+    @JsonProperty("CURRENT")
     CURRENT
 }
